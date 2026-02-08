b27351d6bf2e254d4e37f993a583ec78184c0b9b
Jesse Owen <144388209+jesseowenuk@users.noreply.github.com>
2026-02-08 20:09:13 +0000

    Step 1: Adding the 16-but registers AX, BX, CX & DX

--- emulator.cpp
+++ emulator.cpp
@@ -1,3 +1,15 @@
+#include <stdint.h>
+#include <stdio.h>
+
+typedef struct
+{
+    // 16-bit registers
+    uint16_t AX;
+    uint16_t BX;
+    uint16_t CX;
+    uint16_t DX;
+} CPU16;
+
 int main()
 {
     return 0;
