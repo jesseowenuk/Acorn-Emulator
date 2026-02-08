989d5a4a3608cf823a281490d0c39c652bdfa5a0
Jesse Owen <144388209+jesseowenuk@users.noreply.github.com>
2026-02-08 20:19:37 +0000

    Step 2: Adding the segment offset registers SI, DI, BP & SP

--- emulator.cpp
+++ emulator.cpp
@@ -3,11 +3,19 @@
 
 typedef struct
 {
-    // 16-bit registers
+    // 16-bit registers (general purpose)
+    // These can also be addressed as high or low
+    // eg AX low bytes AL and high bytes AH
     uint16_t AX;
     uint16_t BX;
     uint16_t CX;
     uint16_t DX;
+
+    // These registers are used as offsets into the data space
+    uint16_t SI;    // SI is usually an offset from the DS segment
+    uint16_t DI;    // DI is usually an offset from the ES segment
+    uint16_t BP;    // The stack frame - usually an offset from the stack segment (SS)
+    uint16_t SP;    // The stack pointer - usually an offset from the stack segment (SS)
 } CPU16;
 
 int main()
