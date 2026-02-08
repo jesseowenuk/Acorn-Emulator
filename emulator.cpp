1d972af7693d0456c05330766cef1310137fef92
Jesse Owen <144388209+jesseowenuk@users.noreply.github.com>
2026-02-08 20:32:05 +0000

    Step 4: Adding the flags register

--- emulator.cpp
+++ emulator.cpp
@@ -6,19 +6,28 @@ typedef struct
     // 16-bit registers (general purpose)
     // These can also be addressed as high or low
     // eg AX low bytes AL and high bytes AH
-    uint16_t AX;
-    uint16_t BX;
-    uint16_t CX;
-    uint16_t DX;
+    uint16_t AX;    // Accumulator
+    uint16_t BX;    // Base
+    uint16_t CX;    // Counter
+    uint16_t DX;    // Data
 
     // These registers are used as offsets into the data space
     uint16_t SI;    // SI is usually an offset from the DS segment
     uint16_t DI;    // DI is usually an offset from the ES segment
-    uint16_t BP;    // The stack frame - usually an offset from the stack segment (SS)
+    uint16_t BP;    // Stack base pointer - usually an offset from the stack segment (SS)
     uint16_t SP;    // The stack pointer - usually an offset from the stack segment (SS)
 
     // The instruction pointer
     uint16_t IP;
+
+    // The segments
+    uint16_t CS;
+    uint16_t DS;
+    uint16_t ES;
+    uint16_t SS;
+
+    // Flags
+    uint16_t FLAGS;
 } CPU16;
 
 int main()
