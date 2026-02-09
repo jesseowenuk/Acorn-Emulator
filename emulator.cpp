86060e0585b99385c8fddf6e55187c896c5b368b
Jesse Owen <144388209+jesseowenuk@users.noreply.github.com>
2026-02-09 22:08:24 +0000

    Implemented call and ret instructions plus improved debugging output

--- emulator.cpp
+++ emulator.cpp
@@ -8,6 +8,8 @@ uint8_t memory[MEMORY_SIZE];            // create an array to store our 1MB of R
 
 typedef struct
 {
+    int running;
+
     // 16-bit registers (general purpose)
     // These can also be addressed as high or low
     // eg AX low bytes AL and high bytes AH
@@ -42,12 +44,14 @@ uint16_t read16(uint32_t address);
 void write16(uint32_t address, uint16_t value);
 void push16(CPU16 *cpu, uint16_t value);
 uint16_t pop16(CPU16 *cpu);
+void debug_state(CPU16 *cpu, int show_stack);
 
 // MAIN ////////////////////////////////////////
 int main()
 {
     // Create a CPU and set all the registers to 0
     CPU16 cpu = {0};
+    cpu.running = 1;
 
     // Set up IP and CS
     cpu.CS = 0x0000;
@@ -58,20 +62,21 @@ int main()
     // Write our program to memory
     uint32_t address = 0x2000;
 
+    // CALL to function (offset = 3 bytes ahead)
+    write8(address++, 0xE8); write16(address, 0x0001); address += 2;
+
+    // HLT (fake end)
+    write8(address++, 0xF4); 
+
+    // ---- FUNCTION ------
     // MOV AX, 1234
     write8(address++, 0xB8); write16(address, 0x1234); address += 2;
 
-    // PUSH AX
-    write8(address++, 0x50); 
-
-    // MOV AX, 0
-    write8(address++, 0xB8); write16(address, 0x0000); address += 2;
-
-    // POP AX
-    write8(address++, 0x58); 
+    // RET
+    write8(address++, 0xC3); 
 
     // Fetch / Decode Loop
-    while(1)
+    while(cpu.running)
     {
         // Step 1: Fetch
         uint16_t physical_address = cpu.CS * 16 + cpu.IP;
@@ -158,6 +163,45 @@ int main()
                 break;
             }
 
+            // CALL rel16
+            case 0xE8:
+            {
+                // 1. read 16-bit relative offset after opcode
+                uint16_t offset = read16(cpu.CS * 16 + cpu.IP);
+                cpu.IP += 2;    // move past the operand
+
+                // 2. Push current IP (the return address)
+                push16(&cpu, cpu.IP);
+
+                // 3. Jump to new address
+                cpu.IP += offset;
+
+                #if DEBUG
+                printf("Exeecuted CALL 0x%04X\n", offset);
+                #endif
+                break;
+            }
+
+            // RET
+            case 0xC3:
+            {
+                cpu.IP = pop16(&cpu);
+                #if DEBUG
+                printf("Exeecuted RET\n");
+                #endif
+                break;
+            }
+
+            // HLT
+            case 0xF4:
+            {
+                #if DEBUG
+                printf("CPU halted\n");
+                #endif
+                cpu.running = 0;
+                break;
+            }
+
             default:
             {
                 #if DEBUG
@@ -167,9 +211,7 @@ int main()
             }
         }
 
-        #if DEBUG
-        printf("AX=%04X  SP=%04X  IP=%04X\n", cpu.AX, cpu.SP, cpu.IP);
-        #endif
+        debug_state(&cpu, 1);
     }
 
     return 0;
@@ -219,4 +261,28 @@ uint16_t pop16(CPU16 *cpu)
     uint16_t value = read16(address);
     cpu->SP += 2;
     return value;
+}
+
+// Print debugging information
+void debug_state(CPU16 *cpu, int show_stack)
+{
+    #if DEBUG
+        printf("AX=%04X  BX=%04X  CX=%04X  DX=%04X\n", cpu->AX, cpu->BX, cpu->CX, cpu->DX);
+        printf("CS:IP=%04X:%04X  DS=%04X  ES=%04X  SS:SP=%04X:%04X  FLAGS=%04X\n", cpu->CS, cpu->IP, cpu->DS, cpu->ES, cpu->SS, cpu->SP, cpu->FLAGS);
+
+        if(show_stack)
+        {
+            printf("[STACK] Top 4 bytes:");
+            uint32_t address = cpu->SS * 16 + cpu->SP;
+
+            for(int i = 0; i < 4; i++)
+            {
+                printf(" %02X", memory[address + i]);
+            }
+
+            printf("\n");
+        }
+
+        printf("\n");
+    #endif
 }
